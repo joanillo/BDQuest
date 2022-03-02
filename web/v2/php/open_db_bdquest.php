da8e8ae1448d36aded6bd1fd850a52ccd8ec8f29
joan <joanqc@gmail.com>
2022-03-02 19:48:22 +0100

    220302g

--- web/v2/php/open_db_bdquest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn_bdquest = mysqli_connect("localhost", "bdquest", "keiL2lai");
-if (!$conn_bdquest) {
-    $log->error('Could not connect: ' . mysql_error());
-    die('Could not connect: ' . mysql_error());
-}
-mysqli_select_db($conn_bdquest,"bdquest") or die('Could not select bdquest database.');
-mysqli_set_charset($conn_bdquest, 'utf8');
-?>
--- web/v3/php/open_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "bdquest", "keiL2lai");
-if (!$conn) {
-    $log->error('Could not connect: ' . mysql_error());
-    die('Could not connect: ' . mysql_error());
-}
-mysqli_select_db($conn,"municipis") or die('Could not select municipis database.');
-mysqli_set_charset($conn, 'utf8');
-?>
